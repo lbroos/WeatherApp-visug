70e87b08a983a6e34e9170ee722b434bf9355fc3
lbroos <lindsey@snowball.be>
2024-10-13 15:54:25 +0200

    Add project files.

---
+++ WeatherSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherApp;
+
+public class WeatherSettings
+{
+    public string City { get; set; }
+
+    public int NumberOfDays { get; set; }
+
+    public bool ShowFahrenheit { get; set; }
+}
